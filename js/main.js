f682e4c0ef5058b4047a9979233b0b9dbfc890e8
PoushnayaA <poushnaya@bk.ru>
2023-07-11 12:57:17 +0300

    Убрает закомментированный код

--- js/main.js
+++ js/main.js
@@ -2,6 +2,3 @@ import {objects} from './data.js';
 import {createPictureCards} from './pictures.js';
 
 createPictureCards(objects);
-// console.log(objects);
-
-// console.log(createPictureCards(createAllObjects()));
